c1e08e0a4de7ea44e1365839db975d79b3242ba3
johan855 <johan855@gmail.com>
2020-04-25 21:22:09 +0200

    Added Docker compose

---
+++ producers/models/producer.py
@@ -0,0 +1,35 @@
+import time
+import logging
+
+from confluent_kafka import avro
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.avro import AvroProducer
+
+logger = logging.getLogger(__name__)
+
+
+class Producer:
+    """Defines and provides common functionality amongst Producers"""
+
+    #Track existing topics accross all Producer instances
+    existing_topics = set([])
+
+    def __init__(
+            self,
+            topic_name,
+            key_schema,
+            value_schema=None,
+            num_partitions=1,
+            num_replicas=1,
+    ):
+        """Initializes a Producer object with basic settings"""
+        self.topic_name = topic_name
+        self.key_schema = key_schema
+        self.value_schema = value_schema
+        self.num_partitions = num_partitions
+        self.num_replicas = num_replicas
+
+        """Initializes Broker settings"""
+        self.broker_properties = {
+
+        }
