e2736bb1e1251229d3f1bd9e8817beecadd36e60
johan855 <johan855@gmail.com>
2020-04-28 19:58:17 +0200

    changed station for station name

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -24,8 +24,8 @@ class Turnstile(Producer):
             .replace("-", "_")
             .replace("'", "")
         )
-        turnstile = station.station_id
-        topic_name = f"com.udacity.turnstile.{station_id}"
+        turnstile = station_name
+        topic_name = f"com.udacity.turnstile.{turnstile}"
         super().__init__(
             topic_name,
             key_schema=Turnstile.key_schema,
